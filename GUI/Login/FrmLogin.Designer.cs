e0f551ec73f0333a4c6f1a13f73fbbc3ced90b9d
XBinhs17 <nguyenbinh17062003@gmai.com>
2024-11-05 12:37:24 +0700

    first commit from main

--- GUI/Login/FrmLogin.Designer.cs
+++ GUI/Login/FrmLogin.Designer.cs
@@ -136,16 +136,18 @@
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(88, 199);
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(88, 186);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(367, 22);
+            this.txtPass.Size = new System.Drawing.Size(367, 27);
             this.txtPass.TabIndex = 4;
             // 
             // txtTenDN
             // 
-            this.txtTenDN.Location = new System.Drawing.Point(88, 109);
+            this.txtTenDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenDN.Location = new System.Drawing.Point(88, 96);
             this.txtTenDN.Name = "txtTenDN";
-            this.txtTenDN.Size = new System.Drawing.Size(367, 22);
+            this.txtTenDN.Size = new System.Drawing.Size(367, 27);
             this.txtTenDN.TabIndex = 3;
             // 
             // label3
